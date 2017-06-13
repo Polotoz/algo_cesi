26aac1aaa3f937a7921fbd403e5e1d02cc3caf2c
Paul MARQUIE <paul.marquie@viacesi.fr>
2017-06-13 11:21:31 +0200

    'hanoit'

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ function tourHanoi($nbDisque, $pile1, $pile2, $pile3){
     if($nbDisque == 1){
         echo "On déplace le disque de la pile ".$pile1." vers la pile ".$pile2."<br/>";
     } else {
-        //On déplace nbDisque-1 de la pile 1 vers la pile 2 en passant par la pile 3
+        //On déplace nbDisque-1 de la pile 1 vers la pile 3 en passant par la pile 2
         tourHanoi($nbDisque-1,$pile1, $pile3, $pile2);
-        // On déplace un disque de la pile 1 vers la pile 3
-        tourHanoi(1, $pile1, $pile3, $pile2);
-        //On déplace nbDisque-1 de la pile2 vers la pile 3 en passant par la pile 1
-        tourHanoi($nbDisque-1,$pile2, $pile3, $pile1);
+        // On déplace un disque de la pile 1 vers la pile 2
+        tourHanoi(1, $pile1, $pile2, $pile3);
+        //On déplace nbDisque-1 de la pile3 vers la pile 2 en passant par la pile 1
+        tourHanoi($nbDisque-1,$pile3, $pile2, $pile1);
     }
 }
 
